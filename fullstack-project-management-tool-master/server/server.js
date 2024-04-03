00468098eefeab7e49747fbd62a01420f7e30dad
PJPriyanka147 <priyankajatav07@gmail.com>
2024-04-03 12:32:01 +0530

    updates

--- fullstack-project-management-tool-master/server/server.js
+++ fullstack-project-management-tool-master/server/server.js
@@ -4,35 +4,31 @@ import dotenv from 'dotenv'
 import mongoose from "mongoose";
 import cors from "cors";
 
-dotenv.config()
-mongoose.connect(process.env.MONGODB_PATH, () => {
-}, (e) => console.log(e))
-
-
-
-const PORT = process.env.SERVER_PORT || 9000
-const origin = process.env.CORS_ORIGIN || 'http://localhost:3000'
 
 const app = express()
 
-//API Creation
-
-app.get("/", (req,res)=>{
-    res.send("Express App is Running")
-})
-
-
-app.use(cors({
-    origin:'https://project-planner1203.netlify.app',
-}));
-
-
+dotenv.config()
 
 app.use(express.json())
 app.use(express.urlencoded())
 
-app.use(api)
 
+mongoose.connect(process.env.MONGODB_PATH, () => {
+}, (e) => console.log(e))
+
+app.use(cors({
+    origin: ['https://fashionstore1203.netlify.app','http://localhost:3000']
+  }));
+
+const PORT = process.env.SERVER_PORT || 9000
+
+
+//API Creation
+app.get("/", (req,res)=>{
+    res.send("Express App is Running")
+})
+
+app.use(api)
 
 app.listen(PORT, () => {
     console.log(`Your app is running in http://localhost:${PORT}`)
