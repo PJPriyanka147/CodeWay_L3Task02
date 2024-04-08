481a3edd0b29e050537522dd44f840e33b8dfa2e
PJPriyanka147 <priyankajatav07@gmail.com>
2024-04-08 16:14:17 +0530

    update

--- fullstack-project-management-tool-master/client/src/App.js
+++ fullstack-project-management-tool-master/client/src/App.js
@@ -16,9 +16,10 @@ function App() {
         <Route path="/:projectId" element={<Task />} />
         <Route path="/" element={
           <div className="flex w-full flex-col h-full relative bg-orange-300">
-            <img src={todo} className="absolute top-52 h-96 w-96 right-0" alt="" /> 
+            <img src={todo} className="absolute top-48 h-96 w-96 right-0" alt="" /> 
             <h1 className=" text-indigo-500 top-24 text-6xl font-bold absolute inline pl-8">Project Planner</h1>
-            <p className=" absolute text-gray-400 w-4/5 top-48 text-4xl pl-8">Supercharge Your Productivity with Our Revolutionary Project Planner!!</p>
+            <p className=" absolute text-gray-400 w-4/5 top-48 text-4xl pl-8">"Boost productivity with our revolutionary <br />planner,
+Supercharge your projects <br /> with ease!"</p>
           </div>
         } />
       </Routes>
