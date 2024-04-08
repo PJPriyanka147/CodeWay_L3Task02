fe8de2efacd24ba60656e3ce4552455d15bfe286
PJPriyanka147 <priyankajatav07@gmail.com>
2024-04-08 16:16:37 +0530

    update

--- fullstack-project-management-tool-master/client/src/components/Navbar.js
+++ fullstack-project-management-tool-master/client/src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
        <h1 class="text-3xl font-light text-white py-5 px-5">Welcome Back !</h1>
         <div className="flex gap-5 my-2 mx-5">
           
-            <Link to='/'><button className='w-24 h-10  bg-white outline-none px-2 border border-gray-500 rounded-md text-base'>Sign Out</button></Link>
+            <Link to='/'><button className='w-24 h-10  bg-white outline-none px-2 border border-gray-500 rounded-md text-base'>Back</button></Link>
         </div>
         </div>
   )
